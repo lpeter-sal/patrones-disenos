5d9904e58a4c198088587059d1606c1abf28ad85
Luis P. Salvador <pedrosh09@gmail.com>
2025-04-18 21:24:17 -0700

    Cuarto patron visto: Prototype, teoria y ejercicio practico.

--- 01-creacionales/04-prototype.ts
+++ 01-creacionales/04-prototype.ts
@@ -9,3 +9,47 @@
  * 
  * https://refactoring.guru/es/design-patterns/prototype
  */
+
+
+class CustomDocument {
+    public title: string;
+    private content: string;
+    public author: string;
+
+    constructor(title: string, content: string, author: string) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    // * El objetivo del patron Prototype es crear clones/copias
+    // * Que tiene su propio ADN, con todas sus propiedas y metodos.
+    clone(): CustomDocument {
+        return new CustomDocument(this.title, this.content, this.author);
+    }
+
+    displayInfo() {
+        console.log(`
+            Title: ${this.title}
+            Content: ${this.content}
+            Author: ${this.author}`);
+    }
+}
+
+function main() {
+
+    const document1 = new CustomDocument('Cotizacion', '500 dolares', 'Luis Salvador');
+
+    console.log({ document1 })
+    document1.displayInfo();
+
+
+    const document2 = document1.clone();
+    document2.title = 'Factura';
+
+    console.log({ document2 })
+    document2.displayInfo();
+}
+
+
+main();
--- 01-creacionales/04.2-prototype.ts
+++ 01-creacionales/04.2-prototype.ts
@@ -17,35 +17,74 @@ class Pokemon {
   attacks: string[];
 
   constructor(name: string, type: string, level: number, attacks: string[]) {
-    throw new Error('Method not implemented.');
+    this.name = name;
+    this.type = type;
+    this.level = level;
+    this.attacks = [...attacks]; // * ✅✅✅ Esto es correcto, ya que se pasa por valor  y no por referencia
+
+
+    // ! this.attacks = attacks; ❌❌❌ // ! Esto no es correcto, ya que se pasa por referencia
+    // * Otras alternativas
+    // * this.attacks = [...attacks]; ✅✅✅ // ✅ Esto es correcto, ya que se pasa por valor y no por referencia
+    // * this.attacks = attacks.slice(); ✅✅✅ // ✅ Esto es correcto, ya que se pasa por valor y no por referencia
+    // * this.attacks = attacks.concat(); ✅✅✅ // ✅ Esto es correcto, ya que se pasa por valor y no por referencia
+    // * this.attacks = [...attacks]; ✅✅✅ // ✅ Esto es correcto, ya que se pasa por valor y no por referencia
+    // * this.attacks = attacks.map((attack) => attack); ✅✅✅ // ✅ Esto es correcto, ya que se pasa por valor y no por referencia
   }
 
   // Método para clonar el Pokémon
   clone(): Pokemon {
     // Los ataques deben de evitar pasarse por referencia, es decir, no deben de ser el mismo arreglo.
     // Completar: Debe devolver un nuevo Pokémon con los mismos atributos
+    return new Pokemon(
+      this.name,
+      this.type,
+      this.level,
+      [...this.attacks]
+    );
+
   }
 
   displayInfo(): void {
     console.log(
       `Nombre: ${this.name}\nTipo: ${this.type}\nNivel: ${
         this.level
-      }\nAtaques: ${this.attacks.join(', ')}`
+      }\nAtaques: ${this.attacks.join(', ')}\n`
     );
   }
 }
 
-// Tarea:
-// 1. Crear un Pokémon base.
-// 2. Clonar el Pokémon base y modificar algunos atributos en los clones.
-// 3. Llamar a displayInfo en cada Pokémon para mostrar sus detalles.
+// Tarea: 🆗
+// 1. Crear un Pokémon base. ✅
+// 2. Clonar el Pokémon base y modificar algunos atributos en los clones. ✅
+// 3. Llamar a displayInfo en cada Pokémon para mostrar sus detalles. ✅
 
 // Ejemplo:
-// const basePokemon = new Pokemon("Charmander", "Fuego", 1, ["Llamarada", "Arañazo"]);
-// const clone1 = basePokemon.clone();
-// clone1.name = "Charmeleon";
-// clone1.level = 16;
-// clone1.attacks.push("Lanzallamas");
+// const basePokemon = new Pokemon("Charmander", "Fuego", 1, ["Llamarada", "Arañazo"]); ✅
+// const clone1 = basePokemon.clone(); ✅
+// clone1.name = "Charmeleon"; ✅
+// clone1.level = 16; ✅
+// clone1.attacks.push("Lanzallamas"); ✅
 
-// basePokemon.displayInfo(); // Aquí no debe de aparecer "Lanzallamas"
-// clone1.displayInfo();
+// basePokemon.displayInfo(); // Aquí no debe de aparecer "Lanzallamas" ✅
+// clone1.displayInfo(); ✅
+
+function main() {
+  const basePokemon = new Pokemon(
+    'Charmander',
+    'Fuego',
+    1,
+    ['Llamarada', 'Arañazo']
+  );
+
+  const clone1 = basePokemon.clone();
+  clone1.name = 'Charmeleon';
+  clone1.level = 16;
+  clone1.attacks.push('Lanzallamas');
+
+  basePokemon.displayInfo(); // Aquí no debe de aparecer "Lanzallamas"
+  console.log('--------------------------------\n');
+  clone1.displayInfo(); // Aqui debe de aparecer "Lanzallamas"
+}
+
+main();
