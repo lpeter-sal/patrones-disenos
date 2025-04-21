5841756e88ca2661a2fc3e75e61a2129932e889b
Luis P. Salvador <pedrosh09@gmail.com>
2025-04-21 08:43:12 -0700

    Tercer patron estructural visto: Composite, teoria y ejercicio practico

--- 02-estructurales/03-composite.ts
+++ 02-estructurales/03-composite.ts
@@ -13,3 +13,61 @@
  * https://refactoring.guru/es/design-patterns/composite
  *
  */
+
+interface FileSystemComponent {
+    showDetails(indent?: string): void;
+}
+
+
+class FileExemplo implements FileSystemComponent {
+    constructor(private name: string) {}
+
+    showDetails(indent: string = ''): void {
+        console.log(`${indent} - File: ${this.name}`);
+    }
+}
+
+class Folder implements FileSystemComponent {
+    constructor(private name: string, private contents: FileSystemComponent[] = [] ){ }
+
+    add( component: FileSystemComponent ): void {
+        this.contents.push(component);
+    }
+
+    showDetails(indent: string = ''): void {
+        console.log(`${indent} + Folder: ${this.name}`);
+        if (this.contents) {
+            this.contents.forEach( component => component.showDetails(`${indent}  `));
+        }
+    }
+}
+
+function main() {
+
+    const file1 = new FileExemplo('file1.txt');
+    const file2 = new FileExemplo('file2.txt');
+    const file3 = new FileExemplo('file3.txt');
+    const file4 = new FileExemplo('file4.txt');
+    const file5 = new FileExemplo('file5.txt');
+
+
+    const folder1 = new Folder('folder1', [file1, file2]);
+    const folder2 = new Folder('folder2', [file3]);
+    const folder3 = new Folder('folder3', [file4, file5]);
+    const folder4 = new Folder('folder4', [file1, file2]);
+    const folder5 = new Folder('folder5', [file3]);
+
+    
+    folder2.add(folder3);
+    folder2.add(folder4);
+
+    folder3.add(folder5);
+
+
+    const rootFolder = new Folder('root', [folder1, folder2, folder3]);
+    rootFolder.showDetails();
+
+}
+
+
+main();
--- 02-estructurales/03.2-composite.ts
+++ 02-estructurales/03.2-composite.ts
@@ -16,14 +16,14 @@
 
 import { COLORS } from '../helpers/colors.ts';
 
-// 1. Interfaz MenuComponent
-// Define el método `showDetails`, que implementarán los ítems y categorías de menú.
+// 1. Interfaz MenuComponent 🆗
+// Define el método `showDetails`, que implementarán los ítems y categorías de menú. ✅✅✅
 interface MenuComponent {
   showDetails(indent?: string): void;
 }
 
-// 2. Clase MenuItem
-// Representa un ítem individual del menú, como un platillo o una bebida.
+// 2. Clase MenuItem 🆗
+// Representa un ítem individual del menú, como un platillo o una bebida. ✅✅✅
 class MenuItem implements MenuComponent {
   private name: string;
   private price: number;
@@ -35,36 +35,40 @@ class MenuItem implements MenuComponent {
 
   showDetails(indent: string = ''): void {
     console.log(
-      `${indent}- ${this.name}: %c$${this.price.toFixed(2)}`,
+      `${indent} - ${this.name}: %c$${this.price.toFixed(2)}`,
       COLORS.green
     );
   }
 }
 
-// 3. Clase MenuCategory
-// Representa una categoría de menú que puede contener otros ítems o subcategorías.
+// 3. Clase MenuCategory 🆗
+// Representa una categoría de menú que puede contener otros ítems o subcategorías. ✅✅✅
 class MenuCategory implements MenuComponent {
-  // TODO: Crear dos propiedades privadas: name y items
-  // Name sting y items arreglo de MenuComponent
-  // Name es recibida en el constructor, items se inicializa como un arreglo vacío
 
-  //TODO: Sobrecarga de operadores - Item puede ser MenuComponent o un arreglo de MenuComponent
-  add(item: unknown): void {
-    // TODO: Implementar la sobrecarga de operadores
-    throw new Error('Method not implemented.');
+  constructor(private name: string, private items: MenuComponent[] = []) {}
+
+  add(item: MenuComponent | MenuComponent[] ): void {
+    if( Array.isArray(item) ) {
+      this.items.push(...item);
+      return;
+    }
+
+    this.items.push(item);
   }
 
   showDetails(indent: string = ''): void {
     console.log(`%c${indent}+ ${this.name}`, COLORS.blue);
-    // TODO: Implementar foreach
+    if (this.items) {
+      this.items.forEach( item => item.showDetails(`${indent}  `));
+    }
   }
 }
 
-// 4. Código Cliente para Probar el Composite
+// 4. Código Cliente para Probar el Composite 🆗
 // TODO: en esta función main, no deben de hacer nada, al ejecutarla,
-// Deben de ver la gráfica correcta del menú
+// Deben de ver la gráfica correcta del menú ✅✅✅
 function main() {
-  // Crear ítems individuales
+  // Crear ítems individuales ✅✅✅
   const salad = new MenuItem('Ensalada', 5.99);
   const soup = new MenuItem('Sopa de tomate', 4.99);
   const steak = new MenuItem('Bistec', 15.99);
@@ -72,7 +76,7 @@ function main() {
   const dessert = new MenuItem('Pastel de chocolate', 6.5);
   const coffee = new MenuItem('Café', 1.99);
 
-  // Crear categorías de menú y añadir ítems
+  // Crear categorías de menú y añadir ítems ✅✅✅
   const appetizers = new MenuCategory('Entradas');
   appetizers.add(salad);
   appetizers.add(soup);
@@ -87,14 +91,14 @@ function main() {
   const desserts = new MenuCategory('Postres');
   desserts.add(dessert);
 
-  // Crear un menú principal que contiene todas las categorías
+  // Crear un menú principal que contiene todas las categorías ✅✅✅
   const mainMenu = new MenuCategory('Menú Principal');
   mainMenu.add([appetizers, beverages, desserts, mainCourse]);
   // mainMenu.add(mainCourse);
   // mainMenu.add(beverages);
   // mainMenu.add(desserts);
 
-  // Mostrar la estructura completa del menú
+  // Mostrar la estructura completa del menú ✅✅✅
   console.log('Menú del Restaurante:');
   mainMenu.showDetails();
 }
