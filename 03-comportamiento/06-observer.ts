de5a74f4ba5f89bef65649fe576088951e4de94a
Luis P. Salvador <pedrosh09@gmail.com>
2025-04-25 10:46:33 -0700

    Sexto patron de comportamiento visto: Observer, teoria y ejercicio practico

--- 03-comportamiento/06-observer.ts
+++ 03-comportamiento/06-observer.ts
@@ -13,3 +13,69 @@
  *
  * https://refactoring.guru/es/design-patterns/observer
  */
+
+
+interface Observe {
+    notify(videoTitle:string):void
+}
+
+class YouTubeChannel {
+    private subscribers: Observe[] = [];
+    private name: string;
+
+    constructor(name: string) {
+        this.name = name;
+    }
+
+    subscribe(subscriber: Observe): void {
+        this.subscribers.push(subscriber);
+        console.log(`Nuevo se ha suscrito al canal ${this.name}`);
+    }
+
+    unsubscribe(subscriber: Observe): void {
+        this.subscribers = this.subscribers.filter(
+            obs => obs !== subscriber
+        );
+        console.log(`${ subscriber } se ha desuscrito del canal ${this.name}`);
+    }
+
+    uplaloadVideo(videoTitle: string): void {
+        console.log(`El canal ${this.name} ha subido un nuevo video: ${videoTitle}`);
+        for (const subscriber of this.subscribers) {
+            subscriber.notify(videoTitle);
+        }
+    }
+}
+
+class Subscriber implements Observe {
+    private name: string;
+
+    constructor(name: string) {
+        this.name = name;
+    }
+
+    notify(videoTitle: string): void {
+        console.log(`${this.name} ha sido notificado del nuevo video: ${videoTitle}`);
+    }
+}
+
+function main() {
+    const channel = new YouTubeChannel('Canal de Ejemplo');
+
+    const subscriber1 = new Subscriber('Juan');
+    const subscriber2 = new Subscriber('Maria');
+    const subscriber3 = new Subscriber('Pedro');
+    const subscriber4 = new Subscriber('Luis');
+
+    channel.subscribe(subscriber1);
+    channel.subscribe(subscriber2);
+
+    channel.uplaloadVideo('Nuevo video de ejemplo');
+
+
+
+
+
+}
+
+main();
--- 03-comportamiento/06.2-observer.ts
+++ 03-comportamiento/06.2-observer.ts
@@ -24,13 +24,14 @@ interface Observer {
 // Clase Subject - WeatherStation
 // TODO: Terminal la implementación
 class WeatherStation {
-  // observers = [];
-  // weatherData = 'Soleado';
+  private observers: Observer[] = [];
+  private weatherData: string = 'Soleado';
 
   // Agregar un Observer
   subscribe(observer: Observer): void {
     // TODO: añadir observer
 
+    this.observers.push(observer);
     console.log(
       '%cNueva aplicación suscrita al sistema meteorológico.',
       COLORS.green
@@ -40,7 +41,7 @@ class WeatherStation {
   // Eliminar un Observer
   unsubscribe(observer: Observer): void {
     // TODO: eliminar observer
-
+    this.observers = this.observers.filter( obs => obs !== observer );
     console.log(`%cUna aplicación se ha dado de baja`, COLORS.red);
   }
 
@@ -49,12 +50,16 @@ class WeatherStation {
     console.log(`\nClima actualizado: %c${weatherData}`, COLORS.blue);
 
     // TODO: actualizar clima y notificar a todos los Observers con el método notifyObservers
+    this.weatherData = weatherData;
+    this.notifyObservers();
   }
 
   // Notificar a todos los Observers
   private notifyObservers(): void {
     // TODO: implementar método
-    throw new Error('Method not implemented.');
+    for (const observer of this.observers) {
+      observer.update(this.weatherData);
+    }
   }
 }
 
